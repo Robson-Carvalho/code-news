e44adbb3e90e842f32658967dac3f8cf0bd825fc
Robson-Carvalho <robsoncarvalho.tech@gmail.com>
2023-06-29 20:20:10 -0300

    :sparkles: feat: adding pagination logic

--- js/script.js
+++ js/script.js
@@ -1,5 +1,63 @@
 const url = "https://code-news-api.vercel.app/";
-const containerNews = document.getElementById("container-news");
+const listNews = document.querySelector(".list-news");
+
+const newsByPage = 10;
+const state = {
+  page: 1,
+  newsByPage,
+  totalPage: Math.ceil(100 / newsByPage),
+  maxVisibleButtons: 3,
+};
+
+const html = {
+  get(element) {
+    return document.querySelector(element);
+  },
+};
+
+const update = () => {
+  window.scrollTo({ top: 0, behavior: "smooth" });
+  list.update();
+  buttons.update();
+};
+
+const controls = {
+  next() {
+    if (state.page === state.totalPage) return;
+    state.page++;
+  },
+  prev() {
+    if (state.page === 1) return;
+    state.page--;
+  },
+  goTo(page) {
+    if (page > state.totalPage) {
+      return (state.page = state.totalPage);
+    }
+    if (page < 1) {
+      return (state.page = 1);
+    }
+    state.page = +page;
+  },
+  createListeners() {
+    html.get(".first").addEventListener("click", () => {
+      controls.goTo(1);
+      update();
+    });
+    html.get(".prev").addEventListener("click", () => {
+      controls.prev();
+      update();
+    });
+    html.get(".next").addEventListener("click", () => {
+      controls.next();
+      update();
+    });
+    html.get(".last").addEventListener("click", () => {
+      controls.goTo(state.totalPage);
+      update();
+    });
+  },
+};
 
 const renderNews = (news) => {
   const newsElement = document.createElement("a");
@@ -9,6 +67,7 @@ const renderNews = (news) => {
 
   const imgElement = document.createElement("img");
   imgElement.setAttribute("class", "image-news");
+  imgElement.setAttribute("alt", "Não foi possível carrega a imagem!");
   imgElement.setAttribute("src", `${news.urlToImage}`);
 
   const titleElement = document.createElement("h3");
@@ -23,23 +82,82 @@ const renderNews = (news) => {
   newsElement.appendChild(titleElement);
   newsElement.appendChild(descriptionElement);
 
-  containerNews.appendChild(newsElement);
+  listNews.appendChild(newsElement);
 };
 
-const addNewsOnScreen = ({ result }) => {
-  for (let i = 10; i <= 30; i++) {
-    renderNews(result[i]);
-  }
+const list = {
+  create(item) {
+    renderNews(item);
+  },
+  async update() {
+    const response = await fetch(url);
+    const data = await response.json();
+    const {
+      news: { result },
+    } = await data;
+
+    html.get(".list-news").innerHTML = "";
+    let page = state.page - 1;
+    let start = page * state.newsByPage;
+    let end = start + state.newsByPage;
+
+    const paginatedNews = result.slice(start, end);
+
+    paginatedNews.forEach(list.create);
+  },
 };
 
-async function getNews() {
-  try {
-    const data = await fetch(url).then((res) => res.json());
-    const { news } = await data;
-    addNewsOnScreen(news);
-  } catch (error) {
-    console.log(error);
-  }
-}
+const buttons = {
+  element: html.get(".pagination .numbers"),
+  create(number) {
+    const button = document.createElement("div");
 
-getNews();
+    if (state.page == number) {
+      button.classList.add("active");
+    }
+
+    button.innerText = number;
+    button.addEventListener("click", (event) => {
+      const page = event.target.innerText;
+
+      controls.goTo(page);
+      update();
+    });
+
+    buttons.element.appendChild(button);
+  },
+  update() {
+    buttons.element.innerHTML = "";
+    const { maxLeft, maxRight } = buttons.calculateMaxVisible();
+
+    for (let page = maxLeft; page <= maxRight; page++) {
+      buttons.create(page);
+    }
+  },
+  calculateMaxVisible() {
+    const { maxVisibleButtons } = state;
+    let maxLeft = state.page - Math.floor(maxVisibleButtons / 2);
+    let maxRight = state.page + Math.floor(maxVisibleButtons / 2);
+
+    if (maxLeft < 1) {
+      maxLeft = 1;
+      maxRight = maxVisibleButtons;
+    }
+
+    if (maxRight > state.totalPage) {
+      maxLeft = state.totalPage - (maxVisibleButtons - 1);
+      maxRight = state.totalPage;
+
+      if (maxLeft < 1) maxLeft = 1;
+    }
+
+    return { maxLeft, maxRight };
+  },
+};
+
+const init = () => {
+  update();
+  controls.createListeners();
+};
+
+init();
